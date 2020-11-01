3ffa78df3f882e5f5106aeabd0eeed0687efb926
fajar firmansyah <fajar.vds@gmail.com>
2020-11-01 20:36:10 +0700

    ini adalah Lab1 Biodata

---
+++ lab1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 19:52:46 2020
+
+@author: fajar
+"""
+print("======================")
+print("      BIODATA         ")
+print("======================")
+
+#input
+
+nama = input ("masukan nama: ")
+nama2 = input ("masukan nama panggilan: ")
+NPM = input ("masukan NPM: ")
+tpl = input ("tempat lahir: ")
+tgl = input ("tanggal lahir: ")
+umr = input ("umur: ")
+telepon = input ("Nomor Telepon: ")
+alamat = input ("Alamat Rumah: ")
+
+
+#print Biodata
+
+print("======================")
+print("     BIODATA SAYA     ")
+print("======================")
+
+
+#print
+
+print("Hallo",nama,"saya selalu dipanggil oleh teman teman dengan nama",nama2,
+      "ini adalah NPM saya",NPM,"Saya lahir dari kota",tpl,"tanggal kelahiran saya",tgl
+      ,"Umur saya",umr,"nomor telepon saya",telepon,"alamat rumah saya",alamat)
